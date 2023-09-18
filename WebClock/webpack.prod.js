df92a693c208c7d35259ce11ddeca05905552f38
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-09-18 14:59:11 +0700

    project webclock - menggunakan webpack

---
+++ WebClock/webpack.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'production',
+    module: {
+        rules: [
+            /* babel loader */
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                                '@babel/plugin-transform-runtime',
+                                '@babel/plugin-transform-class-properties'
+                            ]
+                        },
+                    },
+                ],
+            },
+
+        ],
+    },
+});
